431e14e1582174a21e6f3d07f5e6edc066f31a64
max <max@53355a66-a8c0-4aef-80d1-10870d0d3e9b>
2008-04-02 10:37:42 +0000

    handler
    git-svn-id: http://svn.getalime.ru/rails_plugins/attacheable@51 53355a66-a8c0-4aef-80d1-10870d0d3e9b

--- lib/attacheable/photo_handler.rb
+++ lib/attacheable/photo_handler.rb
@@ -37,7 +37,7 @@ module Attacheable
       end
       logger.info "Processed request in #{Time.now - start_time} seconds\n  URI: #{request.params["REQUEST_URI"]}\n\n"
     rescue Exception => e
-      logger.info "!! Internal server error\nURI: #{request.params["REQUEST_URI"]}\n#{e.backtrace.join("\n")}"
+      logger.info "!! Internal server error\nURI: #{request.params["REQUEST_URI"]}\n#{e}\n#{e.backtrace.join("\n")}"
       logger.flush
       response.start(200) do |headers, out|
         headers["Content-Type"] = "text/plain"
