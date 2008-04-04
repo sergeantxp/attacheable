cd83ab7ac07ff96f7afb1b6ef3780fb127b1b208
max <max@53355a66-a8c0-4aef-80d1-10870d0d3e9b>
2008-04-04 07:29:55 +0000

    Added merb upload
    git-svn-id: http://svn.getalime.ru/rails_plugins/attacheable@55 53355a66-a8c0-4aef-80d1-10870d0d3e9b

--- test/attacheable_test.rb
+++ test/attacheable_test.rb
@@ -72,6 +72,19 @@ class AttacheableTest < Test::Unit::TestCase
     assert !File.exists?(File.dirname(__FILE__)+"/public/system/images/0000/0001"), "Directory should be cleaned"
   end
   
+  def test_merb_image_creation
+    path = File.dirname(__FILE__)+"/fixtures/life.jpg"
+    input = {"content_type"=>"image/jpeg", "size"=>File.size(path), "tempfile"=>File.open(path), "filename"=>"life.jpg"}
+    image = Image.new(:uploaded_data => input)
+    assert image.save, "Image should be saved"
+    assert_equal "life", image.attachment_basename
+    assert_equal ".jpg", image.attachment_extname
+    assert File.exists?(File.dirname(__FILE__)+"/public/system/images/0000/0001/life.jpg"), "File should be saved"
+    assert !File.exists?(File.dirname(__FILE__)+"/public/system/images/0000/0001/life_medium.jpg"), "Thumbnails should not be generated"
+    image.destroy
+    assert !File.exists?(File.dirname(__FILE__)+"/public/system/images/0000/0001"), "Directory should be cleaned"
+  end
+  
   def test_image_with_autocreation
     Image.attachment_options[:autocreate] = true
     input = File.open(File.dirname(__FILE__)+"/fixtures/life.jpg")
